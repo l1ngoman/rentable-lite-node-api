1a3c07b820e22304cc420a723b75d8606050cc57
l1ngoman <41375558+l1ngoman@users.noreply.github.com>
2020-08-20 23:05:31 -0700

    ATG:: Fixed customer query JSON results for the API to include the full rental and pickup history of the user with line items. Need to test scaleability and finish logic to determine if line items are active or not based on pickup actual date.

--- api/controllers/customers.js
+++ api/controllers/customers.js
@@ -17,62 +17,59 @@ exports.getAllCustomers = (req, res) => {
 
 exports.getCustomer = (req, res) => {
     const customer_id = parseInt(req.params.id);
-    const sql = `SELECT * FROM customers WHERE customer_id = ${customer_id}`;
-    connection.query(sql, (error, result) => {
-        if(error) {
-            throw error;
-        }
-        // ATG:: NOW QUERY FOR ALL RELATED RENTALS FOR THIS CUSTOMER
-        const sql2 = `  SELECT r.rental_id, r.rental_number, r.rental_date, r.delivery_date, r.rental_status, r.delivery_fee,
-                            rli.rental_item_id, rli.unit_cost, rli.unit_tax_amount,
-                            i.item_id, i.item_name, i.item_description, i.tracking_number, i.serial_number,
-                            p.pickup_id, p.pickup_number, p.pickup_date, p.pickup_actual_date, p.pickup_status 
-                        FROM rentals r
-                        LEFT JOIN rental_line_items rli
-                            ON r.rental_id = rli.rental_id
-                        LEFT JOIN items i
-                            ON rli.item_id = i.item_id
-                        LEFT JOIN pickups p
-                            ON r.rental_id = p.pickup_id
-                        WHERE customer_id = ${customer_id}`;
 
+    // ATG:: NOW QUERY FOR ALL RELATED RENTALS FOR THIS CUSTOMER
+    const sql1 = `  SELECT c.customer_id, c.first_name, c.last_name, c.address_1, c.address_2, c.city, c.state, c.zip, c.phone, c.email,
+                        r.rental_id, r.rental_number, r.rental_date, r.delivery_date, r.rental_status, r.delivery_fee,
+                        rli.rental_item_id, rli.unit_cost, rli.unit_tax_amount,
+                        i.item_id, i.item_name, i.item_description, i.tracking_number, i.serial_number
+                    FROM rentals r
+                    LEFT JOIN customers c
+                        ON r.customer_id = c.customer_id
+                    LEFT JOIN rental_line_items rli
+                        ON r.rental_id = rli.rental_id
+                    LEFT JOIN items i
+                        ON rli.item_id = i.item_id
+                    LEFT JOIN pickups p
+                        ON r.rental_id = p.pickup_id
+                    WHERE r.customer_id = ${customer_id}`;
+    connection.query(sql1, (error1, result1) => {
+        if(error1) {
+            throw error1;
+        }
+        // ATG:: NOW QUERY FOR ALL RELATED PICKUPS FOR THIS CUSTOMER
+        const sql2 = `  SELECT p.pickup_id, p.pickup_number, p.pickup_date, p.pickup_actual_date, p.pickup_status,
+                            r.rental_id, r.rental_number,
+                            pli.pickup_item_id,
+                            i.item_id, i.item_name, i.item_description, i.tracking_number, i.serial_number
+                        FROM pickups p
+                        LEFT JOIN rentals r
+                            ON p.rental_id = r.rental_id
+                        LEFT JOIN pickup_line_items pli
+                            ON p.pickup_id = pli.pickup_id
+                        LEFT JOIN items i
+                            ON pli.item_id = i.item_id
+                        WHERE r.customer_id = ${customer_id}`;
         connection.query(sql2, (error2, result2) => {
             if(error2) {
                 throw error2;
             }
-            // ATG:: NOW QUERY FOR ALL RELATED PICKUPS FOR THIS CUSTOMER
-            const sql3 = `  SELECT p.pickup_id, p.pickup_number, p.pickup_date, p.pickup_actual_date, p.pickup_status,
-                                r.rental_id, r.rental_number,
-                            i.item_id, i.item_name, i.item_description, i.tracking_number, i.serial_number
-                            FROM pickups p
-                            LEFT JOIN rentals r
-                                ON p.rental_id = r.rental_id
-                            LEFT JOIN items i
-                                ON r.item_id = i.item_id
-                            WHERE r.customer_id = ${customer_id}`;
 
-            connection.query(sql3, (error3, result3) => {
-                if(error3) {
-                    throw error3;
-                }
+            // ATG:: CALL PRIVATE FUNCTION TO FORMAT THE RENTAL PORTION OF THE CUSTOMER OBJECT
+            // TAKES THE FIRST QUERY RESULT
+            // RETURNS A CUSTOMER OBJECT WITH FORMATTED RENTAL HISTORY
+            formattedCustomer = _formatRentalJSON(result1);
+            // ATG:: NOW CALL ANOTHER PRIVATE FUNCTION TO FORMAT THE PICKUP PORTION OF THE CUSTOM OBJECT
+            // TAKES THE NEW CUSTOMER OBJECT AND THE SECOND QUERY RESULT
+            // RETURNS THE FINAL CUSTOMER OBJECT WITH FORMATTED RENTAL & PICKUP HISTORY
+            formattedCustomer = _formatPickupJSON(formattedCustomer, result2);
 
-                // NEED TO FIGURE OUT HOW TO SORT THE ARRAYS
-                // let formattedRentals;
-                // if(result2.length > 0) {
-                //     for(let el in result2) {
-                //        result2[el].
-                //     }
-                // }
-                result[0]['rentals'] = result2;
-                result[0]['pickups'] = result3;
-
-                res.status(200).json({
-                    message: `Customer with ID: ${customer_id} successfully retrieved.`,
-                    responseObject: result
-                });  
-            });
-        }); 
-    });
+            res.status(200).json({
+                message: `Customer with ID: ${customer_id} successfully retrieved.`,
+                responseObject: formattedCustomer
+            });  
+        });
+    }); 
 };
 
 exports.createNewCustomer = (req, res) => {
@@ -140,3 +137,182 @@ exports.deleteCustomer = (req, res) => {
         });
     });
 };
+
+/**********************************************************************************/
+/**********************************************************************************/
+/****************************** HELPER FUNCTIONS **********************************/
+/**********************************************************************************/
+/**********************************************************************************/
+
+// ATG:: INTERNAL FUNCTION FOR FORMATTING THE RENTAL JSON WITH LINE ITEMS
+const _formatRentalJSON = function(queryResult) {
+    let formattedCustomer;
+    let formattedRentals = {};
+    let formattedRentalLineItems = {};
+    if(queryResult.length > 0) {
+        for(let el in queryResult) {
+            // ATG:: EXTRACT THE RENTAL ID FROM THE QUERY RESULT
+            let rental_id = queryResult[el].rental_id;
+
+            if(formattedCustomer === undefined) {
+                formattedCustomer = {
+                    customer_id:    queryResult[el].customer_id,
+                    first_name:     queryResult[el].first_name,
+                    last_name:      queryResult[el].last_name,
+                    address_1:      queryResult[el].address_1,
+                    address_2:      queryResult[el].address_2,
+                    city:           queryResult[el].city,
+                    state:          queryResult[el].state,
+                    zip:            queryResult[el].zip,
+                    phone:          queryResult[el].phone,
+                    email:          queryResult[el].email,
+                    rentals:        null,
+                    pickups:        null
+                };
+            }
+
+            // ATG:: IF THE FORMATTED RENTAL OBJECT AT THE RENTAL ID IS UNDEFINED, CREATE THE OBJECT WITH THE PROPERTIES OF THE QUERY RESULTS FROM THIS ITERATION
+            if(formattedRentals[rental_id] === undefined) {
+                formattedRentals[rental_id] = {
+                    rental_id:              queryResult[el].rental_id,
+                    rental_number:          queryResult[el].rental_number,
+                    rental_date:            queryResult[el].rental_date,
+                    delivery_date:          queryResult[el].delivery_date,
+                    rental_status:          queryResult[el].rental_status,
+                    delivery_fee:           queryResult[el].delivery_fee,
+                    items:                  null
+                };
+            }
+
+            // ATG:: IF THE FORMATTED RENTAL LINE ITEMS OBJECT AT THE RENTAL ID IS UNDEFINED AND THE QUERY RESULT FROM THIS ITERATION IS NOT NULL, CREATE ITS FIRST OBJECT WITH THE QUERY RESULTS FROM THIS ITERATION
+            if( (formattedRentalLineItems[rental_id] === undefined) && (queryResult[el].rental_item_id !== null) ) {
+                formattedRentalLineItems[rental_id] = [{
+                    rental_item_id:     queryResult[el].rental_item_id,
+                    unit_cost:          queryResult[el].unit_cost,
+                    unit_tax_amount:    queryResult[el].unit_tax_amount,
+                    item_id:            queryResult[el].item_id,
+                    item_name:          queryResult[el].item_name,
+                    item_description:   queryResult[el].item_description,
+                    tracking_number:    queryResult[el].tracking_number,
+                    serial_number:      queryResult[el].serial_number,
+                    item_status:        'ACTIVE'
+                }];
+            // ATG:: ELSE, STILL CHECKING IF THE QUERY RESULT FROM THIS ITERATION IS NOT NULL, PUSH IN ANOTHER OBJECT USING THE QUERY RESULTS FROM THIS INTERATION
+            } else if(queryResult[el].rental_item_id !== null) {
+                formattedRentalLineItems[rental_id].push({
+                    rental_item_id:     queryResult[el].rental_item_id,
+                    unit_cost:          queryResult[el].unit_cost,
+                    unit_tax_amount:    queryResult[el].unit_tax_amount,
+                    item_id:            queryResult[el].item_id,
+                    item_name:          queryResult[el].item_name,
+                    item_description:   queryResult[el].item_description,
+                    tracking_number:    queryResult[el].tracking_number,
+                    serial_number:      queryResult[el].serial_number,
+                    item_status:        'ACTIVE'
+                });
+            }
+        } // END OF FOR LOOP
+    } // END OF LENGTH > 0
+
+    // ATG:: NOW LOOP THROUGH THE LINE ITEMS TO PUSH THEM INTO THE CORRESPONDING ITEMS ARRAY FOR THEIR RENTAL ID
+    for(let el in formattedRentalLineItems) {
+        formattedRentals[el]['items'] = formattedRentalLineItems[el];
+    }
+
+    // ATG:: NOW LOOP THROUGH THE RENTAL OBJECT OF RENTAL OBJECTS AND PUSH ALL THE INNER OBJECTS INTO AN ARRAY TO BE MORE ACCESSIBLE TO THE USER
+    rentalArray = [];
+    rentalArrayKeys = Object.keys(formattedRentals)
+
+    for (let i = 0; i < rentalArrayKeys.length; i++) {
+        rentalArray.push(formattedRentals[rentalArrayKeys[i]]);
+    }
+
+    // ATG:: ADD THE RENTALS TO THE FORMATTED CUSTOMER OBJECT
+    formattedCustomer['rentals'] = rentalArray;
+
+    return formattedCustomer;
+}
+
+// ATG:: INTERNAL FUNCTION FOR FORMATTING THE PICKUP JSON WITH LINE ITEMS
+const _formatPickupJSON = function(formattedCustomer, queryResult) {
+    let formattedPickups = {};
+    let formattedPickupLineItems = {};
+    if(queryResult.length > 0) {
+        for(let el in queryResult) {
+            // ATG:: EXTRACT THE RENTAL ID FROM THE QUERY RESULT
+            let pickup_id = queryResult[el].pickup_id;
+
+            // ATG:: IF THE FORMATTED RENTAL OBJECT AT THE RENTAL ID IS UNDEFINED, CREATE THE OBJECT WITH THE PROPERTIES OF THE QUERY RESULTS FROM THIS ITERATION
+            if(formattedPickups[pickup_id] === undefined) {
+                formattedPickups[pickup_id] = {
+                    pickup_id:                  queryResult[el].pickup_id,
+                    pickup_number:              queryResult[el].pickup_number,
+                    pickup_date:                queryResult[el].pickup_date,
+                    pickup_actual_date:         queryResult[el].pickup_actual_date,
+                    pickup_status:              queryResult[el].pickup_status,
+                    associated_rental_id:       queryResult[el].rental_id,
+                    associated_rental_number:   queryResult[el].rental_number,
+                    items:                      null
+                };
+            }
+
+            // ATG:: IF THE FORMATTED RENTAL LINE ITEMS OBJECT AT THE RENTAL ID IS UNDEFINED AND THE QUERY RESULT FROM THIS ITERATION IS NOT NULL, CREATE ITS FIRST OBJECT WITH THE QUERY RESULTS FROM THIS ITERATION
+            if( (formattedPickupLineItems[pickup_id] === undefined) && (queryResult[el].pickup_item_id !== null) ) {
+                formattedPickupLineItems[pickup_id] = [{
+                    pickup_item_id:     queryResult[el].pickup_item_id,
+                    item_id:            queryResult[el].item_id,
+                    item_name:          queryResult[el].item_name,
+                    item_description:   queryResult[el].item_description,
+                    tracking_number:    queryResult[el].tracking_number,
+                    serial_number:      queryResult[el].serial_number,    
+                }];
+            // ATG:: ELSE, STILL CHECKING IF THE QUERY RESULT FROM THIS ITERATION IS NOT NULL, PUSH IN ANOTHER OBJECT USING THE QUERY RESULTS FROM THIS INTERATION
+            } else if(queryResult[el].pickup_item_id !== null) {
+                formattedPickupLineItems[pickup_id].push({
+                    pickup_item_id:     queryResult[el].pickup_item_id,
+                    item_id:            queryResult[el].item_id,
+                    item_name:          queryResult[el].item_name,
+                    item_description:   queryResult[el].item_description,
+                    tracking_number:    queryResult[el].tracking_number,
+                    serial_number:      queryResult[el].serial_number,
+                });
+            }
+        } // END OF FOR LOOP
+    } // END OF LENGTH > 0
+
+    // ATG:: NOW LOOP THROUGH THE LINE ITEMS TO PUSH THEM INTO THE CORRESPONDING ITEMS ARRAY FOR THEIR RENTAL ID
+    for(let el in formattedPickupLineItems) {
+        formattedPickups[el]['items'] = formattedPickupLineItems[el];
+    }
+
+    // ATG:: NOW LOOP THROUGH THE PICKUP OBJECT OF PICKUP OBJECTS AND PUSH ALL THE INNER OBJECTS INTO AN ARRAY TO BE MORE ACCESSIBLE TO THE USER
+    pickupArray = [];
+    pickupArrayKeys = Object.keys(formattedPickups)
+
+    for (let i = 0; i < pickupArrayKeys.length; i++) {
+        pickupArray.push(formattedPickups[pickupArrayKeys[i]]);
+    }    
+
+    // ATG:: ADD THE PICKUPS TO THE FORMATTED CUSTOMER OBJECT
+    formattedCustomer['pickups'] = pickupArray;
+
+    // loop through each of the pickups with an pickup_actual_date != null and get their rental_id
+    let rentalIDs = {};
+    for (let i = 0; i < formattedCustomer.pickups.length; i++) {
+        if(formattedCustomer.pickups[i].pickup_actual_date !== null) {
+            rentalIDs[formattedCustomer.pickups[i].associated_rental_id] = [];
+        }
+        // loop through each of the pickup line items to get their item_id
+        for(let el in formattedCustomer.pickups[i].items) {
+            rentalIDs[formattedCustomer.pickups[i].associated_rental_id].push(formattedCustomer.pickups[i].items[el].item_id);
+        }
+    }
+    // now loop through each of the rental line items array and set item_status of 'returned'
+    for (let i = 0; i < formattedCustomers.rentals.length; i++) {
+        if(rentalIDs[formattedCustomers.rentals[i].rental_id]) {
+            formattedCustomers.rentals[i].items[]
+        }
+    }
+
+    return formattedCustomer;
+}
